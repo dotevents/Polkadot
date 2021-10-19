6655fd39eb540d387934b43647d1f7860d53f615
dotevents.co <90984124+dotevents@users.noreply.github.com>
2021-10-19 20:11:10 +0530

    updated

---
+++ src/Components/Footer.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <section className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-lg-10 mx-auto ">
+            <div className="row">
+              <div className="col-6 col-lg-3">
+                <h4>About Us</h4>
+
+                <br />
+
+               
+
+                <br />
+                <br />
+                <a href="">APKMODY's mission is building an open space for MOD APK lovers, Premium APK lovers, and GAMEs lovers.</a>
+                <br />
+                <br />
+
+                
+                <br />
+              </div>
+              <div className="col-6 col-lg-3">
+                <h4>Who we are?</h4>
+
+            
+                <br />
+                <a href="">About</a>
+                <br />
+                <br />
+
+                <a href="">Contact</a>
+                <br />
+                <br />
+                <a href="">Donate</a>
+                <br />
+                <br />
+                <a href="">Terms of services</a>
+                <br />
+                <br />
+                <a href="">Privacy Policy</a>
+                <br />
+                <br />
+              </div>
+              <div className="col-6 col-lg-3">
+                <h4>Useful links
+</h4>
+                <br/>
+               
+                <ul>
+                    <a href="/"><li>hi</li></a>
+                </ul>
+                
+              </div>
+             
+            </div>
+            <hr />
+            <div className="mt-5">
+              <p className="main-hero-para text-center w-100">
+                Copyright @ 2021 basic of crypto. All rights reserved.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Footer;
